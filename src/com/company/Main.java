bc0db5a92f199fc689cefaba85f7bdd4f2f82600
Izabela Szybiak <is113782@stud.ur.edu.pl>
2022-05-09 15:31:55 +0200

    dod. pomieszczenie w main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Pomieszczenie[] mojePomieszczenia = {
+                new Pomieszczenie(){
+                    {
+                        this.szerokosc=3;
+                        this.dlugosc=2;
+                        this.nazwa="pokoj";
+                    }
+                },
+                new Pomieszczenie(){
+                    {
+                        szerokosc=2;
+                        dlugosc=2;
+                        nazwa="lazienka";
+                    }
+                }
+        };
     }
 }
