d5168cd5164f8b663847db81b1e2a40f833b9bb8
Oskar <oskarpasko@gmail.com>
2022-05-09 15:04:30 +0200

    Dodanie Klasy Mieszkanie

---
+++ src/com/company/Mieszkanie.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Mieszkanie implements PowierzchnioLiczone{
+
+    public String wizytowka;
+    //public pomieszczenia;
+
+    @Override
+    public int policzPole() {
+        return 0;
+    }
+}
