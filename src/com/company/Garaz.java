90e81e7def2c76fe4171783f1255722426e66d06
Oskar <oskarpasko@gmail.com>
2022-05-09 15:15:11 +0200

    Dodanie dziedziczenia w klasie Garaz z klasy Pomieszczenie

--- src/com/company/Garaz.java
+++ src/com/company/Garaz.java
@@ -1,6 +1,10 @@
 package com.company;
 
-public class Garaz {
+public class Garaz extends Pomieszczenie{
     int szerokoscBramy;
     int wysokoscBramy;
+
+    public Garaz(int szerokosc, int dlugosc, String nazwa) {
+        super(szerokosc, dlugosc, nazwa);
+    }
 }
