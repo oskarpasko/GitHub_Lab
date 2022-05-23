5951006376a3af181edf7e9e6def0889bc24c011
Oskar <oskarpasko@gmail.com>
2022-05-23 14:32:57 +0200

    Update Pomieszczenie

--- src/com/company/Pomieszczenie.java
+++ src/com/company/Pomieszczenie.java
@@ -1,38 +1,13 @@
 package com.company;
 
-public class Pomieszczenie extends ElementPietra {
+public class Pomieszczenie extends ElementPietra implements PowierzchnioLiczone {
+
     int szerokosc;
     int dlugosc;
     String nazwa;
 
-    public Pomieszczenie(int szerokosc, int dlugosc, String nazwa) {
-        this.dlugosc = dlugosc;
-        this.szerokosc = szerokosc;
-        this.nazwa = nazwa;
-
-    }
-
-    public int getSzerokosc() {
-        return szerokosc;
-    }
-
-    public int getDlugosc() {
-        return dlugosc;
-    }
-
-    public String getNazwa() {
-        return nazwa;
-    }
-
-    public void setSzerokosc(int szerokosc) {
-        this.szerokosc = szerokosc;
-    }
-
-    public void setDlugosc(int dlugosc) {
-        this.dlugosc = dlugosc;
-    }
-
-    public void setNazwa(String nazwa) {
-        this.nazwa = nazwa;
+    @Override
+    public int policzPole() {
+        return szerokosc * dlugosc;
     }
 }
